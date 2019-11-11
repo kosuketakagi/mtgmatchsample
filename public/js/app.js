2c89aa7971dbab854edd9562787760f056730493
kosuketakagi <kousuketakagimail@gmail.com>
2019-11-11 16:28:18 +0900

    7

--- public/js/app.js
+++ public/js/app.js
@@ -49485,9 +49485,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$(function () {
-  alert('sample');
-});
+
 
 /***/ }),
 
--- resources/views/admin/recruit/tweet.blade.php
+++ resources/views/admin/recruit/tweet.blade.php
@@ -4,10 +4,13 @@
 @section('content')
     <div style=" text-align: center;">
     <br>
-  <h2 class="top-mess">募集をツイートしよう！！</h2>
+     <h2 class="top-mess">投稿完了しました。</h2>
+        <br>
+     <h2 class="top-mess">募集をツイートしよう！！</h2>
 
     <a  class="tweet-button" href="https://twitter.com/intent/tweet?text=『 {{ $recruit->title}}』MTG Matchにて募集中！！&url=https://mtgmatch.sugarlessmtg.com/detail?id={{ $recruit->id}}&via=MatchMtg" target=”_blank”>Tweet</a>
-<br><br>
+    <br>
+    <br>
   <a href="/admin/recruit/mypage">My募集ページへ</a>
 
     </div>
